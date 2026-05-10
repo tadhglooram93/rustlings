359e5badea039a8df1628b1283e313f62c0998dc
Tadhg Looram <tadhg.looram@gmail.com>
2026-05-10 13:35:39 +0000

    Add boolean variable tests and negation cases in primitive_types1.rs

--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -1,14 +1,10 @@
-// Booleans (`bool`)
-
 fn main() {
     let is_morning = true;
+    let is_evening = !is_morning;
     if is_morning {
         println!("Good morning!");
     }
 
-    // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
-    // The value of the variable should be the negation (opposite) of `is_morning`.
-    // let …
     if is_evening {
         println!("Good evening!");
     }
