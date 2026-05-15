7bfb62f13e2161facefe5457d96a47fab7718350
Tadhg Looram <tadhg.looram@gmail.com>
2026-05-15 13:39:11 +0000

    Add tuple indexing example for primitive_types6.rs

--- exercises/04_primitive_types/primitive_types6.rs
+++ exercises/04_primitive_types/primitive_types6.rs
@@ -8,9 +8,8 @@ mod tests {
     fn indexing_tuple() {
         let numbers = (1, 2, 3);
 
-        // TODO: Use a tuple index to access the second element of `numbers`
-        // and assign it to a variable called `second`.
-        // let second = ???;
+        // Access the second element of the tuple directly by index
+        let second = numbers.1;
 
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
     }
