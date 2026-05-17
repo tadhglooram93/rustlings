0f6228821ca42377c481cd5c6b3be63b9537dd9c
Tadhg Looram <tadhg.looram@gmail.com>
2026-05-17 13:35:33 +0000

    Implement vecs2 with ownership, loops, slices, and vectors

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -2,8 +2,7 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
 
     for element in input {
-        // TODO: Multiply each element in the `input` slice by 2 and push it to
-        // the `output` vector.
+        output.push(element * 2);
     }
 
     output
