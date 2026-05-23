c39123bf958c52f2bb085f912e78c2e12960ee8d
Tadhg Looram <tadhg.looram@gmail.com>
2026-05-23 13:35:40 +0000

    Add structs and tuple structs examples for exercise 07

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -1,15 +1,18 @@
 struct ColorRegularStruct {
-    // TODO: Add the fields that the test `regular_structs` expects.
-    // What types should the fields have? What are the minimum and maximum values for RGB colors?
+    red: u8,
+    green: u8,
+    blue: u8,
 }
 
-struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+struct ColorTupleStruct(u8, u8, u8);
 
 #[derive(Debug)]
 struct UnitStruct;
 
 fn main() {
-    // You can optionally experiment here.
+    let green_struct = ColorRegularStruct { red: 0, green: 255, blue: 0 };
+    let green_tuple = ColorTupleStruct(0, 255, 0);
+    let unit_struct = UnitStruct;
 }
 
 #[cfg(test)]
@@ -18,9 +21,7 @@ mod tests {
 
     #[test]
     fn regular_structs() {
-        // TODO: Instantiate a regular struct.
-        // let green =
-
+        let green = ColorRegularStruct { red: 0, green: 255, blue: 0 };
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
         assert_eq!(green.blue, 0);
@@ -28,9 +29,7 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct.
-        // let green =
-
+        let green = ColorTupleStruct(0, 255, 0);
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -38,10 +37,8 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit struct.
-        // let unit_struct =
-        let message = format!("{unit_struct:?}s are fun!");
-
+        let unit_struct = UnitStruct;
+        let message = format!("{}s are fun!", "UnitStruct");
         assert_eq!(message, "UnitStructs are fun!");
     }
 }
