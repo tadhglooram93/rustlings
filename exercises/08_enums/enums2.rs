7ad67b5351942c87723a870c1d7048279a98df59
Tadhg Looram <tadhg.looram@gmail.com>
2026-05-27 14:13:22 +0000

    Implement enums and pattern matching in enums2.rs

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -6,21 +6,22 @@ struct Point {
 
 #[derive(Debug)]
 enum Message {
-    // TODO: Define the different variants used below.
+    Resize { width: u64, height: u64 },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
     fn call(&self) {
-        println!("{self:?}");
+        println!("{:?}", self);
     }
 }
 
 fn main() {
     let messages = [
-        Message::Resize {
-            width: 10,
-            height: 30,
-        },
+        Message::Resize { width: 10, height: 30 },
         Message::Move(Point { x: 10, y: 15 }),
         Message::Echo(String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
