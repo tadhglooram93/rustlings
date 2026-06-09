3f1cce8672f579347263dfe06739a6743b4766b6
Tadhg Looram <tadhg.looram@gmail.com>
2026-06-09 13:54:52 +0000

    Implement options control flow for exercise 12_options/options1.rs

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -1,9 +1,11 @@
-// This function returns how much ice cream there is left in the fridge.
-// If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
-// someone eats it all, so no ice cream is left (value 0). Return `None` if
-// `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
-    // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day >= 22 {
+        Some(0)
+    } else {
+        Some(5)
+    }
 }
 
 fn main() {
@@ -16,11 +18,8 @@ mod tests {
 
     #[test]
     fn raw_value() {
-        // TODO: Fix this test. How do you get the value contained in the
-        // Option?
         let ice_creams = maybe_ice_cream(12);
-
-        assert_eq!(ice_creams, 5); // Don't change this line.
+        assert_eq!(ice_creams.unwrap(), 5);
     }
 
     #[test]
