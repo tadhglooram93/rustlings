67ee204c69905a8065cf442edb158b1a995722ad
Tadhg Looram <tadhg.looram@gmail.com>
2026-06-18 14:05:45 +0000

    Add generics example with type inference to exercise 14

--- exercises/14_generics/generics1.rs
+++ exercises/14_generics/generics1.rs
@@ -1,12 +1,6 @@
-// `Vec<T>` is generic over the type `T`. In most cases, the compiler is able to
-// infer `T`, for example after pushing a value with a concrete type to the vector.
-// But in this exercise, the compiler needs some help through a type annotation.
-
 fn main() {
-    // TODO: Fix the compiler error by annotating the type of the vector
-    // `Vec<T>`. Choose `T` as some integer type that can be created from
-    // `u8` and `i8`.
-    let mut numbers = Vec::new();
+    // Specify the type `T` for the vector as `i16`, which can be created from both `u8` and `i8`
+    let mut numbers: Vec<i16> = Vec::new();
 
     // Don't change the lines below.
     let n1: u8 = 42;
@@ -14,5 +8,5 @@ fn main() {
     let n2: i8 = -1;
     numbers.push(n2.into());
 
-    println!("{numbers:?}");
+    println!("{:?}", numbers);
 }
