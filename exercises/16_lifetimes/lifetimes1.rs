9089aac575b959363d3c767fc1ea9b1cdc3f9c37
Tadhg Looram <tadhg.looram@gmail.com>
2026-06-26 13:46:18 +0000

    Add lifetime annotations to functions in lifetimes1.rs

--- exercises/16_lifetimes/lifetimes1.rs
+++ exercises/16_lifetimes/lifetimes1.rs
@@ -3,8 +3,8 @@
 // going out of scope before it is used. Remember, references are borrows and do
 // not own their own data. What if their owner goes out of scope?
 
-// TODO: Fix the compiler error by updating the function signature.
-fn longest(x: &str, y: &str) -> &str {
+// The fix is to give the function a lifetime parameter to connect the lifetimes of the inputs and output.
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
