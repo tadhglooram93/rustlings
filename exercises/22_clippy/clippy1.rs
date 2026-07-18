fdeaed8ff9425d99e10df2786c6c2b1fe7fbabde
Tadhg Looram <tadhg.looram@gmail.com>
2026-07-18 14:21:18 +0000

    Add constants and floating point usage example in clippy1.rs

--- exercises/22_clippy/clippy1.rs
+++ exercises/22_clippy/clippy1.rs
@@ -5,8 +5,8 @@
 // warnings. Check Clippy's suggestions from the output to solve the exercise.
 
 fn main() {
-    // TODO: Fix the Clippy lint in this line.
-    let pi = 3.14;
+    // Fixed: use the constant `std::f32::consts::PI` instead of a literal.
+    let pi = std::f32::consts::PI;
     let radius: f32 = 5.0;
 
     let area = pi * radius.powi(2);
